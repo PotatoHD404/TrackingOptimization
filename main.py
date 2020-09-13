68f12e79969a1daced7278990d63266d8957d7e9
PotatoHD1 <31506993+PotatoHD1@users.noreply.github.com>
2020-09-13 15:45:06 +0300

    Сделал алгоритмы трекинга

--- main.py
+++ main.py
@@ -1,54 +1,96 @@
-import numpy as np
 import cv2
 import sys
 
-video_path = 'M6 Motorway Traffic.mp4'
-cv2.ocl.setUseOpenCL(False)
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+print(major_ver, minor_ver, subminor_ver)
+if __name__ == '__main__':
 
-version = cv2.__version__.split('.')[0]
-print
-version
+    # Set up tracker.
+    # Instead of MIL, you can also use
 
-# read video file
-cap = cv2.VideoCapture(video_path)
+    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[2]
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+        if tracker_type == 'MOSSE':
+            tracker = cv2.TrackerMOSSE_create()
+        if tracker_type == "CSRT":
+            tracker = cv2.TrackerCSRT_create()
 
-# check opencv version
-if version == '2':
-    fgbg = cv2.BackgroundSubtractorMOG2()
-if version == '3':
-    fgbg = cv2.createBackgroundSubtractorMOG2()
+    # Read video
+    video = cv2.VideoCapture(r"C:\Users\korna\Downloads\Televzr Downloads\M6 Motorway Traffic [PNCJQkvALVc].mp4")
 
-while (cap.isOpened):
+    # Exit if video not opened.
+    if not video.isOpened():
+        print
+        "Could not open video"
+        sys.exit()
 
-    # if ret is true than no error with cap.isOpened
-    ret, frame = cap.read()
+    # Read first frame.
+    ok, frame = video.read()
+    if not ok:
+        print
+        'Cannot read video file'
+        sys.exit()
 
-    if ret == True:
+    # Define an initial bounding box
+    bbox = (287, 23, 86, 320)
 
-        # apply background substraction
-        fgmask = fgbg.apply(frame)
+    # Uncomment the line below to select a different bounding box
+    bbox = cv2.selectROI(frame, False)
 
-        # check opencv version
-        if version == '2':
-            (contours, hierarchy) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        if version == '3':
-            (im2, contours, hierarchy) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Initialize tracker with first frame and bounding box
+    ok = tracker.init(frame, bbox)
 
-        # looping for contours
-        for c in contours:
-            if cv2.contourArea(c) < 500:
-                continue
-
-            # get bounding box from countour
-            (x, y, w, h) = cv2.boundingRect(c)
-
-            # draw bounding box
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        cv2.imshow('foreground and background', fgmask)
-        cv2.imshow('rgb', frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+    while True:
+        # Read a new frame
+        ok, frame = video.read()
+        if not ok:
             break
 
-cap.release()
-cv2.destroyAllWindows()
+        # Start timer
+        timer = cv2.getTickCount()
+
+        # Update tracker
+        ok, bbox = tracker.update(frame)
+
+        # Calculate Frames per second (FPS)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+        # Draw bounding box
+        if ok:
+            # Tracking success
+            p1 = (int(bbox[0]), int(bbox[1]))
+            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+        else:
+            # Tracking failure
+            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+        # Display tracker type on frame
+        cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+
+        # Display FPS on frame
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+
+        # Display result
+        cv2.imshow("Tracking", frame)
+
+        # Exit if ESC pressed
+        k = cv2.waitKey(1) & 0xff
+        if k == 27: break
+
+# https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/
